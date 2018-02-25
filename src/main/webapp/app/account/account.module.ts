b9e9608f92db858012456810b11c21432a6da72b
vagrant <vagrant@vagrant.vm>
2018-02-25 18:51:13 +0000

    Generated with JHipster 4.14.0

--- src/main/webapp/app/account/account.module.ts
+++ src/main/webapp/app/account/account.module.ts
@@ -22,7 +22,7 @@ import {
 @NgModule({
     imports: [
         MaterialAndStockManagementSharedModule,
-        RouterModule.forRoot(accountState, { useHash: true })
+        RouterModule.forChild(accountState)
     ],
     declarations: [
         ActivateComponent,
